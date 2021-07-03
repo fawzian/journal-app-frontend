8fc14d38369afee1cb003cb2f9462956df78f519
fawzian <fawzianur@gmail.com>
2021-07-03 08:27:40 -0400

    local storage set up

--- src/Pages/Auth.js
+++ src/Pages/Auth.js
@@ -19,6 +19,8 @@ const Auth = (props) => {
             console.log(userData)
             const {token, user } = userData;
             dispatch({ type: "auth", payload: {token, username: user.username} });
+            window.localStorage.setItem("auth", JSON.stringify({ token, username: user.username }));
+            props.history.push("/dashboard");
         }
     }, [userData]);
 
